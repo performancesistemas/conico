e6290bda075ee4a2d9ef7c7a9f16d734798c1bbe
Diego Correa <diegomartincorrea@gmail.com>
2018-03-14 10:03:24 -0300

    Add bootstrap 4 layout

--- typings/cordova-typings.d.ts
+++ typings/cordova-typings.d.ts
@@ -1,5 +1,1 @@
-
 /// <reference path="../.vscode/typings/cordova/cordova.d.ts"/>
-/// <reference path="../.vscode/typings/cordova/plugins/StatusBar.d.ts"/>
-/// <reference path="../.vscode/typings/cordova/plugins/Push.d.ts"/>
-/// <reference path="../.vscode/typings/cordova/plugins/Dialogs.d.ts"/>
