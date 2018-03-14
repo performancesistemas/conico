b52b4739a54dc6d877ea710431a8836a78128c9b
Diego Correa <diegomartincorrea@gmail.com>
2018-03-13 21:48:48 -0300

    Works with JQUERY Mobile

---
+++ typings/cordova-typings.d.ts
@@ -0,0 +1,5 @@
+
+/// <reference path="../.vscode/typings/cordova/cordova.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/StatusBar.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Push.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Dialogs.d.ts"/>
